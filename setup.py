b0a0b152e0201c3f9a8f287413b24f5d4d633ade
Garvin Kruthof <GarvinK@users.noreply.github.com>
2022-01-29 21:55:19 +0100

    v0.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="refinda",
-    version="0.1",
+    version="0.11",
     description="A Python package for reproducing financial data",
     url="https://github.com/kruthof/refinda",
     author="Garvin Kruthof",
