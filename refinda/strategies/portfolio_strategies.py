f1c96ea164977339f9d874af6f51c480b5dd2398
Garvin Kruthof <GarvinK@users.noreply.github.com>
2022-11-05 11:20:18 +0100

    add minVar strategy and rename file

--- refinda/strategies/portfolio_strategies.py
+++ refinda/strategies/portfolio_strategies.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import scipy.optimize as sco
 
 def strategy_1n(data,funds = 1000000, transaction_costs=0.001):
     '''
@@ -58,3 +59,58 @@ def strategy_1n(data,funds = 1000000, transaction_costs=0.001):
 
     funds_investment['returns']  = funds_investment.funds.pct_change()
     return funds_investment.set_index('date')
+
+def get_minVar_Portfolio(data,window,rf_exclude=True):
+    '''
+    Function generates covariance matrix based on data of shape(window,...)
+    Covariance is "out-of-sample", it includes not the latest date
+    e.g. the covariance for 01.01.2022 is based on data until 31.12.2021.
+
+    @param: data dataframe column based data
+    @param: window int integer indicating the length of the window, for which the
+            cov matrix should be based on
+    @return cov_list dataframe with array of covariances per date. date as index
+    '''
+
+    date = np.array(data['date'].copy())
+
+    if rf_exclude:
+        data = data.iloc[:,(data.columns != 'RF') & (data.columns != 'date')]
+    else:
+        data = data.iloc[:,data.columns != 'date']
+
+    #calculate percentage change
+    data = data.pct_change()
+    #create empty df
+    minVar_weights = pd.DataFrame()
+
+    #Prepare optimizer
+    #  create a sequence of (min, max) pairs
+    bounds = tuple((0, 1) for w in range(len(data.columns)))
+    # anonymous lambda function
+    constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+    # Repeat the list with the value (1 / 12) 12 times, and convert list to array
+    equal_weights = np.array([1 / len(data.columns)] * len(data.columns))
+    #loop through each observation and calculate min var weights using the last n=window observations
+    for i in range(data.shape[0]-window):
+        # Minimization results
+        _minVar = sco.minimize(
+          # Objective function
+          fun = lambda weights: np.sqrt(np.transpose(weights) @ (data.iloc[i:i+window].cov() * 253/window) @ weights),
+          # Initial guess, which is the equal weight array
+          x0 = equal_weights,
+          method = 'SLSQP',
+          bounds = bounds,
+          constraints = constraints
+        )['x']
+        #append result to dataframe
+        minVar_weights = minVar_weights.append(pd.Series(_minVar),ignore_index=True)
+        #returns.append(np.dot(_returns,_minVar).sum())
+
+
+    minVar = minVar_weights.iloc[0:len(date)]
+    minVar.columns = data.columns #rename columns
+    minVar['date']= np.array(date[0+window:len(date) ]) #include date again
+
+
+    return minVar.set_index('date') #set date as index and return
